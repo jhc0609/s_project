f7503ee8f0cab20284a061ec233dab8923346f75
Haechan Jo <jhc0609@kaist.ac.kr>
2023-02-24 03:59:29 +0900

    2nd commit

--- app.py
+++ app.py
@@ -1,33 +1,71 @@
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
-client = MongoClient('mongodb://localhost', 27017)
+from flask_restx import Api, Resource, fields
+
+client = MongoClient('mongodb://test:test@3.38.94.94', 27017)
+
+client = MongoClient('mongodb://localhost:5000, 27017')
 
 db = client.project
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+api = Api(app,
+          version="1.0",
+          title="Server Storage Service",
+          description="Manage metadata")
+
+ns = api.namespace('metadata', description='Project APIs')
+model = api.model('Metadata Model',
+                  {
+                      'id': fields.String(required=True,
+                                          description="ID of the metadata",
+                                          help="ID cannot be blank."),
+                      'created_datetime': fields.String(required=True,
+                                            description="created_datetime of the metadata",
+                                            help="cannot be blank."),
+                      'inference_result': fields.String(required=False,
+                                           description="inference_result of the metadata",
+                                           help="can be blank."),
+                       'file_path': fields.String(required=False,
+                                           description="file_path of the metadata",
+                                           help="can be blank."),
+                       'type': fields.String(required=False,
+                                           description="type of the metadata",
+                                           help="can be blank."),
+                  })
+
+members = dict()
 
 
-@app.route('/test', methods=['GET'])
-def test_get():
-    id = request.args.get('id')
-    data_list = list(db.metadata.find({"id":id}, {'_id': False}))
-    return jsonify({'metadata': data_list})
+@ns.route("/")
+class MemberList(Resource):
+    def get(self):
+        """
+        returns list of metadata
+        """
+        data_list = list(db.metadata.find({}, {'_id': False}))
+        return data_list
+
+    @ns.expect(model)
+    def post(self):
+        """
+        Adds a new metadata to the list
+        """
+        body = request.json
+        db.metadata.insert_one(body)
 
 
-@app.route('/test', methods=['POST'])
-def test_post():
-    id = request.form['id']
-    created_datetime = request.form['created_datetime']
-    inference_result = request.form['inference_result']
-    file_path = request.form['file_path']
-    type = request.form['type']
-    doc = {'id': id, 'created_datetime' : created_datetime, 'inference_result' : inference_result, 'file_path' : file_path, 'type' : type}
-    db.metadata.insert_one(doc)
-    return jsonify({'msg': '저장 완료'})
+@ns.route("/<id>")
+class Member(Resource):
+    @ns.doc(responses={200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error'},
+             params={'id': 'Specify the Id associated with the metadata'})
+    def get(self, id):
+        """
+        get a metadata
+        """
+        data_list = list(db.metadata.find({"id":id}, {'_id': False}))
+        return data_list
 
 
 if __name__ == '__main__':
